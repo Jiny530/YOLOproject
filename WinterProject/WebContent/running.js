e065b860df15d167a081b110f4c972c233ae8c9f
MinjungShin <ydaispaxero@naver.com>
2020-01-29 10:28:55 +0900

    running 수정

--- WinterProject/WebContent/running.js
+++ WinterProject/WebContent/running.js
@@ -17,7 +17,7 @@ var config = {
 };
         
 var player;
-    
+var limit2;
 
 var platforms;
 var cursors;
@@ -31,17 +31,20 @@ var game = new Phaser.Game(config);
 function preload ()
 {   
     //640,360
-    this.load.image('background', 'assets/background4.png');
-    this.load.image('bg', 'assets/bg5.png');
-    this.load.image('ground2','assets/ground4.png');
-    //this.load.image('hi','assets/limit2.png');
-    this.load.image('heart', 'assets/heart.png');
-    //this.load.image('banana', 'assets/banana.png');
-    //this.load.image('rock', 'assets/rock.png');
-    //this.load.image('cloud', 'assets/cloud.png');
-    //this.load.image('star', 'assets/star.png');
-    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
-    //this.load.image('ground', 'assets/platform.png');
+    this.load.image('background', 'assets/running/background4.png');
+    this.load.image('bg', 'assets/running/bg5.png');
+    this.load.image('ground2','assets/running/ground4.png');
+    //this.load.image('hi','assets/running/limit2.png');
+    this.load.image('heart', 'assets/running/heart.png');
+    //this.load.image('banana', 'assets/running/banana.png');
+    //this.load.image('rock', 'assets/running/rock.png');
+    //this.load.image('cloud', 'assets/running/cloud.png');
+    //this.load.image('star', 'assets/running/star.png');
+    this.load.spritesheet('dude', 'assets/running/dude.png', { frameWidth: 32, frameHeight: 48 });
+    //this.load.image('ground', 'assets/running/platform.png');
+    this.load.image('cat', 'assets/running/cat.png');
+    this.load.image('cloud', 'assets/running/cloud3.png');
+    this.load.image('limit', 'assets/running/limit2.png');
  }
         
 function create ()
@@ -52,14 +55,21 @@ function create ()
     this.add.image(384,256,'bg');
     //this.add.image(384, 256, 'hi');
     platforms.create(384, 426, 'ground2');
+    
             
             
     //this.add.image(384,256,'heart');
     //this.add.image(384,256,'star');
     this.add.image(663,20,'heart');
+    this.add.image(200,160,'cloud');
+    this.add.image(360,200,'cloud');
+    this.add.image(580,180,'cloud');
+    this.add.image(200,300,'cat');
+    
         
 
     player = this.physics.add.sprite(100, 300, 'dude');
+    //limit2= this.physics.add.sprite(384, 256, 'dude');
         
     //  Player physics properties. Give the little guy a slight bounce.
     player.setBounce(0.2);
@@ -90,7 +100,7 @@ function create ()
     //  Input Events
     cursors = this.input.keyboard.createCursorKeys();
     this.physics.add.collider(player, platforms);
-    //this.physics.add.collider(player, limit);
+    //this.physics.add.collider(player, limit2);
             
         
     //  The score
