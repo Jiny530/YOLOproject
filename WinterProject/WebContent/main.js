415585cf46c21dbe459443b304d0cdfe28abb0bd
sojung127 <ssojung127@gmail.com>
2020-02-03 13:04:05 +0900

    아이콘 클릭 가능하도록 수정

--- WinterProject/WebContent/main.js
+++ WinterProject/WebContent/main.js
@@ -21,20 +21,39 @@ function preload ()
     this.load.image('running','assets/running/cat.PNG');
     this.load.image('store24','assets/store24/과자_포카칩.PNG')
 }
-
+var blackjack
+var pizza
+var running
+var store
 function create ()
 {
     blackjack=this.add.image(100,250,'blackjack');
     blackjack.scale=0.2
+    blackjack.setInteractive();
+    blackjack.on('pointerdown',function(pointer){
+        if(!click_stop) click_stop=true;
+    });
 
     pizza=this.add.image(200,250,'pizza')
     pizza.scale=0.5
+    pizza.setInteractive();
+    pizza.on('pointerdown',function(pointer){
+        if(!click_stop) click_stop=true;
+    });
 
     running=this.add.image(300,250,'running')
     running.scale=2
+    running.setInteractive();
+    running.on('pointerdown',function(pointer){
+        if(!click_stop) click_stop=true;
+    });
 
     store=this.add.image(400,250,'store24')
     store.scale=0.1
+    store.setInteractive();
+    store.on('pointerdown',function(pointer){
+        if(!click_stop) click_stop=true;
+    });
 }
 function update (){
 
